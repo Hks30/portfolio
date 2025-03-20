e104c8a2b73f8334c626a4ae22a3ed53d8933d1e
Hks30 <tur08798@temple.edu>
2025-03-20 17:13:08 -0400

    package changed for deployment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import StoryMap from './components/StoryMap/StoryMap';
 import DetailPage from './components/StoryMap/DetailPage';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<StoryMap />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<StoryMap />} />
+      <Route path="/detail/:id" element={<DetailPage />} />
+    </Routes>
   );
 };
 
