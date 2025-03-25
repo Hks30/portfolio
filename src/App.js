7c48ac9ca4bfd5ea2aa0d4f6ef6bb1fb9abe4dc1
Hks30 <tur08798@temple.edu>
2025-03-25 17:11:32 -0400

    UI fixed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import StoryMap from './components/StoryMap/StoryMap';
 import DetailPage from './components/StoryMap/DetailPage';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #root {
+    background-color: #0c0e1a;
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
+  
+  body {
+    overflow-x: hidden;
+  }
+`;
 
 const App = () => {
   return (
-    
-    <Routes>
-      <Route path="/" element={<StoryMap />} />
-      <Route path="/detail/:id" element={<DetailPage />} />
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<StoryMap />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+      </Routes>
+    </>
   );
 };
 
